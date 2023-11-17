323b511a7d3c4199ed231af937fa6f11edd00ac4
kyw0716 <kyw0716@naver.com>
2023-11-17 15:20:46 +0900

    feat: 뉴욕 타임즈 아티클 검색 요청 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+'use client';
+
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const queryClient = new QueryClient();
+
 export default function Home() {
   return (
-    <main>
-      <div>하이</div>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main>
+        <Test />
+      </main>
+    </QueryClientProvider>
   );
+}
+
+function Test() {
+  const { data } = useQuery({
+    queryKey: ['search-articles'],
+    queryFn: async () => {
+      const response = await axios.get(
+        `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${process.env.NEXT_PUBLIC_NEW_YORK_TIMES_API_KEY}&fq=glocations:("SOUTH KOREA")`
+      );
+
+      return response.data;
+    },
+  });
+
+  console.log(data);
+
+  return <div>하이</div>;
 }
